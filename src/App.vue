ed1b965237e97c5a888a37f65d592f3221e520e8
haru1843 <haru1843inf@gmail.co>
2020-03-20 22:23:25 +0900

    授業ページの基礎実装

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,101 @@
 <template>
-  <div id="app">
+  <div id="header">
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <b-navbar toggleable="xl" type="dark" variant="dark" fixed="top">
+        <!-- ロゴなど -->
+        <b-navbar-brand to="/">西森研究室</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <!-- 各項目へのリンク -->
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item-dropdown text="Lab" right>
+              <b-dropdown-item to="/Lab">Contents</b-dropdown-item>
+              <b-dropdown-item to="/Lab">Member</b-dropdown-item>
+              <b-dropdown-item to="/Lab">History</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item to="/Class">Class</b-nav-item>
+            <b-nav-item-dropdown text="Q&A" right>
+              <b-dropdown-item to="/Q&A">Answer</b-dropdown-item>
+              <b-dropdown-item to="/Q&A">Send</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item to="/Link">Link</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- 右側の要素 -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-text>ここに書くこと無い?</b-nav-text>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <router-view />
-    <div></div>
   </div>
 </template>
 
 <style lang="stylus">
-#app {
+@import url('https://fonts.googleapis.com/css?family=Kosugi+Maru&display=swap&subset=japanese');
+font-KM = 'Kosugi Maru'
+hl-fs = 30px
+hl-line-px = 3px
+hl-line-color = #008554
+
+.load-str
+  font-family: font-KM, sans-serif;
+  color: #AAAAAA;
+  font-size: 30px
+
+.headline {
+  font-family: font-KM, sans-serif;
+  font-size: hl-fs;
+  position: relative;
+  line-height: 1.4;
+  padding: 0.25em 1em;
+  margin: 10px 0px 50px
+  display: inline-block;
+}
+
+.headline:before, .headline:after {
+  content:'';
+  width: 20px;
+  height: 30px;
+  position: absolute;
+  display: inline-block;
+}
+
+.headline:before {
+  border-left: solid hl-line-px hl-line-color;
+  border-top: solid hl-line-px hl-line-color;
+  top:0;
+  left: 0;
+}
+
+.headline:after {
+  border-right: solid hl-line-px hl-line-color;
+  border-bottom: solid hl-line-px hl-line-color;
+  bottom:0;
+  right: 0;
+}
+
+#header {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 100px;
 }
+
+.md-block
+  padding: 0.5em 1em;
+  margin: 2em 0;
+  background: white;
+  border-top: solid 5px #5d627b;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.22);
+
+  h1
+    margin: 40px 0px 10px;
+    padding: 0;
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,110 @@
 <template>
   <div class="home">
-    <b-img
+    <header class="header">
+      <div>
+        <img fluid alt="Vue logo" src="../assets/lab_logo_top.png" />
+      </div>
+      <!-- <b-img
       fluid
       alt="Vue logo"
       src="https://www.niigata-u.ac.jp/wp-content/uploads/2019/04/emain_color.jpg"
-    />
-    時間に応じて一言
-    <div>
-      <b-button
-        variant="outline-secondary"
-        href="https://github.com/haru1843"
-        target="_blank"
-      >
-        Github
-      </b-button>
-      <b-button
-        variant="outline-success"
-        href="https://qiita.com/haru1843"
-        target="_blank"
-      >
-        Qiita
-      </b-button>
-      <b-button
-        variant="outline-info"
-        href="https://twitter.com/haru1843inf"
-        target="_blank"
-      >
-        Twitter
-      </b-button>
-      <b-button
-        variant="outline-danger"
-        href="https://atcoder.jp/users/haru1843"
-        target="_blank"
-      >
-        AtCoder
-      </b-button>
-    </div>
+      />-->
+    </header>
+    <main class="main">
+      <div>時間に応じて一言</div>
+    </main>
+    <aside class="aside">aside</aside>
+    <nav class="nav">nav</nav>
+    <footer class="footer">
+      footer
+      <div>
+        <b-button
+          variant="outline-secondary"
+          href="https://github.com/haru1843"
+          target="_blank"
+          >Github</b-button
+        >
+        <b-button
+          variant="outline-success"
+          href="https://qiita.com/haru1843"
+          target="_blank"
+          >Qiita</b-button
+        >
+        <b-button
+          variant="outline-info"
+          href="https://twitter.com/haru1843inf"
+          target="_blank"
+          >Twitter</b-button
+        >
+        <b-button
+          variant="outline-danger"
+          href="https://atcoder.jp/users/haru1843"
+          target="_blank"
+          >AtCoder</b-button
+        >
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home"
-  // components: {
-  //   HelloWorld
-  // }
 };
 </script>
+
+<style>
+.home {
+  display: grid;
+  /* 1列目から順番に180px、1fr、160pxの幅 */
+  grid-template-columns: 180px 1fr 160px;
+  /* 1行目から順番に60px 1fr 90pxの高さ */
+  grid-template-rows: 360px 1fr 90px;
+}
+
+.header {
+  /* 列の1本目から4本目のグリッドラインまで */
+  grid-column-start: 1;
+  grid-column-end: 4;
+  /* 行の1本目から2本目のグリッドラインまで */
+  grid-row-start: 1;
+  grid-row-end: auto;
+  padding-top: 10px;
+}
+
+.main {
+  /* 列の2本目から3本目のグリッドラインまで */
+  grid-column-start: 2;
+  grid-column-end: auto;
+  /* 行の2本目から3本目のグリッドラインまで */
+  grid-row-start: 2;
+  grid-row-end: auto;
+}
+
+.aside {
+  /* 列の1本目から2本目のグリッドラインまで */
+  grid-column-start: 1;
+  grid-column-end: auto;
+  /* 行の2本目から3本目のグリッドラインまで */
+  grid-row-start: 2;
+  grid-row-end: auto;
+}
+
+.nav {
+  /* 列の3本目から4本目のグリッドラインまで */
+  grid-column-start: 3;
+  grid-column-end: auto;
+  /* 行の2本目から3本目のグリッドラインまで */
+  grid-row-start: 2;
+  grid-row-end: auto;
+}
+
+.footer {
+  /* 列の1本目から4本目のグリッドラインまで */
+  grid-column-start: 1;
+  grid-column-end: 4;
+  /* 行の3本目から4本目のグリッドラインまで */
+  grid-row-start: 3;
+  grid-row-end: auto;
+}
+</style>
