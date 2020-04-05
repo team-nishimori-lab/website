e667241bb39412340915e7243e53b8cbb3762334
haru1843 <haru1843inf@gmail.co>
2020-04-05 21:37:01 +0900

    lab-activityの年間予定表の雛形作成

--- src/views/LabContents.vue
+++ src/views/LabContents.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="lab-contents">
-    <div class="headline">
-      研究内容
-    </div>
-    <div id="bg"></div>
-    <p class="scroll-Y" :class="{ sticky: position > 0 }">{{ scrollY }}</p>
-
-    <!-- <div style="height: 300vh"></div> -->
+    <div class="header-space"></div>
+    西森研究室では MIMO (Multiple Input Multiple Output)技術を用いて,
+    無線LAN・携帯電話への適用を目指した研究を行っています.
   </div>
 </template>
 
@@ -14,39 +10,15 @@
 export default {
   name: "Lab_Contents",
   data() {
-    return {
-      scrollY: 0
-    };
+    return {};
   },
-  mounted() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
-  methods: {
-    handleScroll() {
-      this.scrollY = window.scrollY;
-    }
-  }
+  mounted() {},
+  methods: {}
 };
 </script>
 
-<style scoped lang="stylus">
-.lab-contents {
-  text-align: center;
-}
-
-#bg {
-  // background-repeat: no-repeat;
-  width: 100%;
-  height: 300vh;
-  padding-top: calc(300 / 1000 * 100%); /* calc(画像高さ ÷ 画像横幅 × 100%) */
-  // padding-top: 100vh;
-  // background: url("https://source.unsplash.com/user/unsplash/") center center / cover no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-  // background: url(bg.jpg) center center / cover no-repeat;
-}
-
-.scroll-Y {
-  position: fixed;
+<style scoped lang="scss">
+.header-space {
+  height: 10vh;
 }
 </style>
