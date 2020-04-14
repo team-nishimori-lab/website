906fc17a43f42e589d64fa1d3a4df81ea28492d5
haru1843 <haru1843inf@gmail.co>
2020-04-14 11:34:26 +0900

    画像の軽量化

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
+import BootstrapVue from "bootstrap-vue";
+import VueSmoothScroll from "vue2-smooth-scroll";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VueSmoothScroll);
 
 new Vue({
   el: "#app",
